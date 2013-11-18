c9565b5a0fcd72170c1d91b7da0de3ce6de9c498
Andrew Dixon <dixon@atomicbroadcast.net>
2013-11-18 11:13:27 -0800

    Update Styling

--- Gemfile
+++ Gemfile
@@ -2,10 +2,12 @@ source 'https://rubygems.org'
 
 gem 'rails', '4.0.0'
 gem 'devise', '3.2.0'
+gem 'haml-rails'
 gem 'sass-rails', '4.0.1'
 gem 'bootstrap-sass', '3.0.0.0'
 gem 'jquery-rails', '3.0.4'
 gem 'uglifier'
+gem 'turbolinks'
 
 group :development, :test do
   gem 'sqlite3'
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,4 @@
 Hikuter::Application.routes.draw do
   devise_for :users
-
-#  devise_for :users, :skip => [:registrations]
-#
-#  devise_for :users, :controllers => {
-#    :registrations => "registrations"
-#  }
-
-  authenticated :user do
-    devise_scope :user do
-      root to: "registrations#show", :as => "profile"
-    end
-  end
-
-  unauthenticated do
-    devise_scope :user do
-      root to: "registrations#new", :as => "unauthenticated"
-    end
-  end
+  root to: 'pages#home'
 end
